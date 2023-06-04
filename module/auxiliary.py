776003de5f4bf00d701d308cbc7e806ed55fb0ec
Coaixy <coaixy@outlook.com>
2023-06-04 09:32:42 +0800

    Fix the bug that the cookie is not filled in and the error is reported

--- module/auxiliary.py
+++ module/auxiliary.py
@@ -4,21 +4,40 @@
 import time
 import re
 import json
+from typing import Match
 
-def getTimeStamp(ms: bool=False) -> int:
+
+def getTimeStamp(ms: bool = False) -> int:
     if ms:
         return int(time.time() * 1000)
     return int(time.time())
 
-def isEnglish(keyword: str) -> bool:
+
+def isEnglish(keyword: str) -> Match[str] | None:
     return re.match(r'[a-zA-Z]', keyword)
 
-def getCookie(filePath: str, names: list=[]) -> dict:
+
+def getCookie(filePath: str, names: list = []) -> dict:
     cookie_ = {}
-    with open(filePath) as file:
-        cookies = json.load(file)
-        for cookie in cookies:
-            name = cookie['name']
-            if name in names:
-                cookie_[name] = cookie['value']
+    if isEmpty(filePath):
+        return cookie_
+    try:
+        with open(filePath) as file:
+            cookies = json.load(file)
+            for cookie in cookies:
+                name = cookie['name']
+                if name in names:
+                    cookie_[name] = cookie['value']
+    except Exception:
+        pass
+
     return cookie_
+
+
+def isEmpty(filePath: str) -> bool:
+    f = open(filePath)
+    text = f.read()
+    if text == "":
+        return True
+    else:
+        return False
