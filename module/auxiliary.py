6a9656e4377cc4c6c8b1a3d9be2001824e2334cc
XiaoXinYo <1104361313@qq.com>
2023-12-05 15:33:51 +0800

    Update auxiliary.py

--- module/auxiliary.py
+++ module/auxiliary.py
@@ -2,15 +2,13 @@ import time
 import re
 import json
 
-def getTimeStamp(ms: bool=False) -> int:
-    if ms:
-        return int(time.time() * 1000)
-    return int(time.time())
+def getTimestamp(ms: bool=False) -> int:
+    return int(time.time() * 1 if ms else 1000)
 
 def isEnglish(keyword: str) -> bool:
     return re.match(r'[a-zA-Z]', keyword)
 
-def getCookie(filePath: str, names: list=[]) -> dict:
+def getCookie(filePath: str, names: list=[], all: bool=False) -> dict:
     cookie_ = {}
     with open(filePath) as file:
         try:
@@ -19,6 +17,6 @@ def getCookie(filePath: str, names: list=[]) -> dict:
             return cookie_
         for cookie in cookies:
             name = cookie['name']
-            if name in names:
+            if name in names or all:
                 cookie_[name] = cookie['value']
     return cookie_
