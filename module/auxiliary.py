cc619369266cc9591287b96aa915c9a36a73db4e
XiaoXinYo <1104361313@qq.com>
2024-01-14 13:40:04 +0800

    Update auxiliary.py

--- module/auxiliary.py
+++ module/auxiliary.py
@@ -1,22 +1,21 @@
+from typing import Union
 import time
-import re
 import json
 
 def getTimestamp(ms: bool=False) -> int:
     return int(time.time() * (1000 if ms else 1))
 
-def isEnglish(keyword: str) -> bool:
-    return re.match(r'[a-zA-Z]', keyword)
-
-def getCookie(filePath: str, names: list=[], all: bool=False) -> dict:
-    cookie_ = {}
+def getCookie(filePath: str, dict_: bool=True) -> Union[dict, str]:
+    result = {}
     with open(filePath) as file:
         try:
             cookies = json.load(file)
         except:
-            return cookie_
+            return result
+
         for cookie in cookies:
-            name = cookie['name']
-            if name in names or all:
-                cookie_[name] = cookie['value']
-    return cookie_
+            result[cookie['name']] = cookie['value']
+
+    if not dict_:
+        result = '; '.join([f'{name}={value}' for name, value in result.items()])
+    return result
